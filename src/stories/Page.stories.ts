df5e897e1b5e4f009c70b1c7ed578b9a8fbe2173
yasuaki640 <yasuaki640@gmail.com>
2023-06-21 21:00:51 +0900

    run prettier

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,20 +1,20 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
-import { within, userEvent } from '@storybook/testing-library';
-import MyPage from './Page.vue';
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { within, userEvent } from "@storybook/testing-library";
+import MyPage from "./Page.vue";
 
 const meta = {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: MyPage,
   render: () => ({
     components: { MyPage },
-    template: '<my-page />',
+    template: "<my-page />",
   }),
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/vue/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 } satisfies Meta<typeof MyPage>;
 
 export default meta;
@@ -24,7 +24,7 @@ type Story = StoryObj<typeof meta>;
 export const LoggedIn: Story = {
   play: async ({ canvasElement }: any) => {
     const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
+    const loginButton = await canvas.getByRole("button", {
       name: /Log in/i,
     });
     await userEvent.click(loginButton);
